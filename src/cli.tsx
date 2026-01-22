bceb99618f4887489fd97cbafb201dcd6d959ca3
Yazan ALBaiz <yazan.albaiz@zid.sa>
2026-01-22 22:12:06 +0300

    chore: increment npm version number to 0.1.3 and version to 1.0.3 (im still figuring things out)

--- src/cli.tsx
+++ src/cli.tsx
@@ -20,7 +20,7 @@ import { logger, setDebugEnabled } from './lib/logger.js';
 import { setNotificationsEnabled, setSoundEnabled } from './lib/notifications.js';
 
 // Package version (will be replaced by build)
-const VERSION = '1.0.2';
+const VERSION = '1.0.3';
 
 // CLI program
 const program = new Command();
